b7742c5441ee90b67fc0ae89511a77f5d688735e
Sandeep Soni <sandeep.soni@gmail.com>
2022-12-15 18:16:13 +0530

    only user and hostname

--- bash_functions.sh
+++ bash_functions.sh
@@ -73,7 +73,8 @@ function ssh_ip {
 }
 
 function set_win_title(){
-    echo -ne "\033]0; $USER on $HOSTNAME, at $(basename "$PWD") \007"
+    echo -ne "\033]0; ${USER}@${HOSTNAME} \007"
+    # echo -ne "\033]0; $USER on $HOSTNAME, at $(basename "$PWD") \007"
 }
 
 starship_precmd_user_func="set_win_title"
